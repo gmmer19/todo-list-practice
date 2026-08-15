261fb3792f83629ce67b95ff99f6d7f3de1dc5c1
gmmer19 <geometrydash8961@gmail.com>
2026-08-15 17:26:23 +0300

    Initial commit: Todo list with vanilla JS

---
+++ script.js
@@ -0,0 +1,135 @@
+const addButton = document.querySelector("#addButton");
+const itemInput = document.querySelector("#itemInput");
+const itemList = document.querySelector("#itemList");
+const searchInput = document.querySelector("#searchInput");
+const items = [];
+
+const savedData = localStorage.getItem("items");
+if (savedData) {
+	items.push(...JSON.parse(savedData));
+}
+
+const savedSearch = localStorage.getItem("search");
+if (savedSearch) {
+	searchInput.value = savedSearch;
+	renderItems();
+}
+
+function createItem(item) {
+	const li = document.createElement("li");
+	const span = document.createElement("span");
+	const button = document.createElement("button");
+	const checkbox = document.createElement("input");
+
+	checkbox.type = "checkbox";
+	checkbox.checked = item.completed;
+
+	span.textContent = item.text;
+	button.textContent = "Удалить";
+
+	li.dataset.id = item.id;
+	li.append(checkbox);
+	li.append(span);
+	li.append(button);
+
+	itemList.append(li);
+}
+
+items.forEach(function (item) {
+	createItem(item);
+});
+
+function renderItems() {
+	const inputText = searchInput.value;
+	const filteredItems = items.filter((item) =>
+		item.text.toLowerCase().includes(inputText.toLowerCase()),
+	);
+	itemList.innerHTML = "";
+	filteredItems.forEach(function (item) {
+		createItem(item);
+	});
+}
+searchInput.addEventListener("input", function (event) {
+	renderItems();
+	localStorage.setItem("search", searchInput.value);
+});
+
+itemList.addEventListener("click", function (event) {
+	const li = event.target.parentElement;
+	const id = Number(li.dataset.id);
+
+	if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+		const item = items.find((item) => item.id === id);
+		item.completed = event.target.checked;
+		localStorage.setItem("items", JSON.stringify(items));
+	}
+
+	if (event.target.tagName === "BUTTON") {
+		const index = items.findIndex((item) => item.id === id);
+		items.splice(index, 1);
+		localStorage.setItem("items", JSON.stringify(items));
+		li.remove();
+	}
+});
+
+itemList.addEventListener("dblclick", function (event) {
+	if (event.target.tagName !== "SPAN") {
+		return;
+	}
+	let editing = true;
+	const span = event.target;
+	const li = span.parentElement;
+	const id = Number(li.dataset.id);
+
+	const item = items.find((item) => item.id === id);
+
+	const input = document.createElement("input");
+	input.value = item.text;
+	span.replaceWith(input);
+	input.focus();
+
+	function finishEditing() {
+		if (!editing) {
+			return;
+		}
+		if (input.value.trim() === "") {
+			return;
+		}
+		editing = false;
+		item.text = input.value;
+		localStorage.setItem("items", JSON.stringify(items));
+
+		input.replaceWith(span);
+		span.textContent = item.text;
+	}
+
+	input.addEventListener("keydown", function (event) {
+		if (event.key === "Enter") {
+			finishEditing();
+		} else if (event.key === "Escape") {
+			editing = false;
+			input.replaceWith(span);
+		}
+	});
+
+	input.addEventListener("blur", function () {
+		finishEditing();
+	});
+});
+
+addButton.addEventListener("click", function () {
+	const text = itemInput.value;
+
+	if (text.trim() === "") {
+		return;
+	}
+	const item = {
+		id: Date.now(),
+		text: text,
+		completed: false,
+	};
+	items.push(item);
+	localStorage.setItem("items", JSON.stringify(items));
+	createItem(item);
+	itemInput.value = "";
+});
